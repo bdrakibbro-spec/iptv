46e065f3808eed95a08a80a18a57472f824d281b
bdrakibbro-spec <bdrakibbro@gmail.com>
2026-07-17 06:24:19 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,22 @@
 <?php
-$host = "mysql-2fa06d22-bdrakibbro-08e1.l.aivencloud.com"; 
-$db_user = "avnadmin"; 
-$db_pass = "AVNS_8S16f59r-lgKycT_uxL"; 
-$db_name = "defaultdb"; 
-$port = "14644"; 
+$host = 'mysql-2fa06d22-bdrakibbro-08e1.l.aivencloud.com';
+$port = 14648;
+$user = 'avnadmin';
+$pass = 'AVNS_8S16f59r-lgKycT_uxL';
+$dbname = 'defaultdb';
 
-// ডাটাবেজ কানেকশন তৈরি
-$conn = new mysqli($host, $db_user, $db_pass, $db_name, $port);
+// SSL মোডে কানেক্ট করার জন্য mysqli অবজেক্ট তৈরি করা
+$conn = mysqli_init();
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!$conn) {
+    die("mysqli_init failed");
+}
+
+// SSL রিকোয়ারমেন্ট সেট করা (Aiven-এর জন্য এটি বাধ্যতামূলক)
+$conn->ssl_set(NULL, NULL, NULL, NULL, NULL);
+
+// ডাটাবেজের সাথে কানেক্ট করা
+if (!$conn->real_connect($host, $user, $pass, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Connect Error: " . mysqli_connect_error());
 }
 ?>
